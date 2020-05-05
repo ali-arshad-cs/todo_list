034405bb09ff4e311f259c47ce2a9ffc6a0adf99
Ali <aliarshad.cs@gmail.com>
2020-05-06 04:42:26 +0500

    changes added to deploy to heroku

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # todo_list
+
+
+
--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't0%!ujb01xb*gui^lz@o!^pavnnog_st$t95#&x=fqx1rb)(@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-list-3.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
